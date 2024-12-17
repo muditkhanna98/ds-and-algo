5d610acc49f951d76ebdb427d1d4dab290ed440c
Mudit Khanna <muditkhanna998@gmail.com>
2024-12-17 10:36:59 -0500

    insert at index in doubly linkedlist

--- src/linkedList/doublyLinkedList/DoublyLinkedList.java
+++ src/linkedList/doublyLinkedList/DoublyLinkedList.java
@@ -4,8 +4,29 @@ public class DoublyLinkedList {
     Node head;
 
     public void insertAtIndex(int index, int value) {
+        Node node = new Node(value);
 
+        if (index == 0) {
+            node.next = head;
+            if (head != null) {
+                head.prev = node;
+            }
+            head = node;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                Node b = current.next;
 
+                current.next = node;
+
+                if (b != null) {
+                    b.prev = node;
+                }
+
+                node.next = b;
+                node.prev = current;
+            }
+        }
     }
 
     public void printValues() {
--- src/linkedList/doublyLinkedList/Main.java
+++ src/linkedList/doublyLinkedList/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList list = new DoublyLinkedList();
         list.insertAtIndex(0, 1);
-        list.insertAtIndex(1, 2);
-
+        list.insertAtIndex(2, 2);
         list.printValues();
     }
 }
