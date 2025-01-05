c80966e85d6a47fad09a996f1ae9546500a1e204
Mudit Khanna <muditkhanna998@gmail.com>
2025-01-05 16:44:35 -0500

    Add enqueue and dequeue functions to circular queue

--- src/queues/CircularQueue.java
+++ src/queues/CircularQueue.java
@@ -1,5 +1,38 @@
 package queues;
 
 public class CircularQueue {
+    private static final int MAX_SIZE = 101;
+    private int[] arr = new int[MAX_SIZE];
+    private int front = 0;
+    private int rear = -1;
+    private int currentSize = 0;
 
+    private boolean isFull() {
+        return this.currentSize == this.MAX_SIZE;
+    }
+
+    private boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public void enqueue(int item) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return;
+        }
+        rear = (rear + 1) % this.MAX_SIZE;
+        arr[rear] = item;
+        currentSize++;
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int element = arr[front];
+        front = (front + 1) % this.MAX_SIZE;
+        currentSize--;
+        return element;
+    }
 }
